498c84f3c1d20aaa1173a502131a8d9ca20d7c41
AndreMooore <143398486+AndreMooore@users.noreply.github.com>
2024-04-02 14:37:19 +0200

    Prøver

--- oblig4/src/App.jsx
+++ oblig4/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
-
+import { Home } from './components/Home'
+import { Layout } from './components/Layout'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+
+
 
 function App() {
   
 
   return (
     <>
-      
+      <Layout>
+      <Routes>
+          <Route index element={<Home />}/>
+        </Routes>
+      </Layout>
+
     </>
   )
 }
