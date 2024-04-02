c20523828eff427b2a653cbee4be1a6b9582a27f
AndreMooore <143398486+AndreMooore@users.noreply.github.com>
2024-04-02 17:01:59 +0200

    ko

---
+++ oblig4/src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Layout({children}){
+
+    return (
+        <>
+        <header>
+            <h1>hei</h1>
+        </header>
+            <section>
+               {children}
+            </section>
+        </>
+    )
+}
